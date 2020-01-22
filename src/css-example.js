113864ce028e3be6d03979550396933d0968b3e8
Chris Villa <chrisvilla@me.com>
2020-01-22 20:52:16 +0000

    feat: more sensible default template

--- src/css-example.js
+++ src/css-example.js
@@ -1,67 +1,19 @@
-export default `
-.Background {
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  z-index: -1;
-}
-
-.Main {
-  background: lightgray;
-  font-family: "Avenir Next";
-  position: relative;
-  width: 100%;
-  height: 100%;
-  z-index: -2;
-}
-
-.Inner {
+export default `.body {
   align-items: center;
+  background-image: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.50) 100%), url("https://source.unsplash.com/WLUHO9A_xik");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-origin: center;
+  color: white;
   display: flex;
-  width: 100%;
+  font-family: Arial;
   height: 100%;
-}
-
-.Content {
-  padding: 32px;
-  padding-top: 16px;
-}
-
-.Eyebrow {
-  font-weight: 600;
-  margin-bottom: 8px
-}
-
-h1, h2 {
-  margin: 0;
-  padding: 0;
-  font-weight: 400;
-  line-height: 1.2;
+  justify-content: center;
 }
 
 h1 {
-  font-size: 48px;
+  font-size: 96px;
   font-weight: 700;
-  margin-bottom: 8px;
-}
-
-.Watermark {
-  position: absolute;
-  font-size: 18px;
-  opacity: 0.8;
-  left: 32px;
-  bottom: 32px;
-}
-
-.Watermark-logo {
-  font-weight: 700;
-}
-
-body, html {
-  height: 100%;
-  padding: 0;
   margin: 0;
 }
 `;
